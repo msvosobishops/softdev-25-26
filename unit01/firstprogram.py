d91bbf1266f78920fbd4cd0bea2bcc40970010d9
msvosobishops <dominique.voso@bishops.com>
2025-08-25 12:45:00 -0700

    first commit

---
+++ unit01/firstprogram.py
@@ -0,0 +1,20 @@
+'''
+Name, Date, Description
+Dominique Voso, 8/25/25, first python program
+'''
+
+def main():
+    # code will go here, indented
+    print("hello world!")
+
+    # make variables 
+    last_name = "Voso"
+    email = "dominique.voso@bishops.com"
+    birth_date = 12
+
+    # print with variables
+    print(f"{last_name} has the email address {email}")
+
+
+# main call goes here, unindented
+main()
---
+++ unit01/welcome.py
@@ -0,0 +1,15 @@
+'''
+Ms. Voso 8/25
+A program that greets the user
+'''
+
+def main():
+    # the input function allows us to get input from the user 
+    name = input("What is your name? ")
+    grade = input("What is your grade? ")
+
+    # greet the user
+    print(f"Hello {name}! Nice to meet you! Welcome to {grade}th grade.")
+
+
+main()
