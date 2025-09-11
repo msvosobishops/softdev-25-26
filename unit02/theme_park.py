09f3f47a941e1d2fa534b3792536c435af4875d8
msvosobishops <dominique.voso@bishops.com>
2025-09-11 12:44:27 -0700

    theme_park update

--- unit02/theme_park.py
+++ unit02/theme_park.py
@@ -10,18 +10,22 @@ def main():
     adult_present = input("Do you have an adult with you? ")
     heart_condition = input("Do you have a heart condition? ")
 
-    # Under 12 or under 42 in
+    # Under 12 or under 42 in can't ride
     if age < 12 or height < 42:
         print("Sorry, you cannot ride the roller coaster")
-    # Over 65 and heart condition 
+    
+    # Over 65 and heart condition can't ride
     elif age > 65 and heart_condition == "yes": 
         print("Sorry, you cannot ride the roller coaster")
+    
     # Between 42 to 47 inch needs an adult presence
     elif 42 <= height <= 47 and adult_present == "yes":
         print("You may ride the roller coaster with an adult")
-    # Over 48 inches
+    
+    # Over 48 inches can ride
     elif height >= 48:
         print("You may ride the roller coaster alone!")
+    
     # Anyone else cannot ride 
     else:
         print("Sorry, you cannot ride the roller coaster")
