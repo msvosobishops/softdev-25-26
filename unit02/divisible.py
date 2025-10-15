6b6235b390d77b9b78595460e520abdbcfc0b282
msvosobishops <dominique.voso@bishops.com>
2025-10-15 10:42:39 -0700

    while

--- unit02/divisible.py
+++ unit02/divisible.py
@@ -1,21 +1,22 @@
 '''
-Check if a number is divisible by 2, 3, both, or neither
+Program to check if a number is divisible by 2, 3, both, or neither
 '''
 
 def main():
-    # get number
-    num = int(input("Number: "))
+    # get input 
+    num = int(input("Give me a number: "))
 
-    # if divisible by 2 and 3
+    # check divisibility 
+    # divisible by both
     if num % 2 == 0 and num % 3 == 0:
-        print(f"{num} is divisible by both 2 and 3")
+        print(f"{num} is divisible by BOTH 2 and 3")
     # divisible by 2 or 3 individually
     elif num % 2 == 0:
         print(f"{num} is divisible by 2")
     elif num % 3 == 0:
         print(f"{num} is divisible by 3")
-    # otherwise its not divisible by 2 or 3
+    # not divisible by either
     else:
-        print(f"{num} is NOT divisible by 2 or 3")
+        print(f"{num} is not divisible by either 2 or 3")
 
 main()
--- unit02/triangle.py
+++ unit02/triangle.py
@@ -1,24 +1,24 @@
 '''
-Program to ask for 3 sides of a triangle and classify it
+Get three sides from a user and classify the triangle by sides
 '''
 
 def main():
+    # get inputs
     side1 = float(input("Side 1: "))
     side2 = float(input("Side 2: "))
     side3 = float(input("Side 3: "))
 
-    # First check if triangle is possible 
-    if side1 + side2 <= side3 or side1 + side3 <= side2 or side2 + side3 <= side1:
-        print("The triangle is NOT POSSIBLE")
-    # equilateral
+    # check if triangle is not possible
+    if side1 + side2 <= side3 or side2 + side3 <= side1 or side1 + side3 <= side2:
+        print("Triangle is not possible")
+    # equilateral 
     elif side1 == side2 == side3:
-        print("The triangle is equilateral")
-    # isosceles 
+        print("Equilateral")
+    # isosceles
     elif side1 == side2 or side2 == side3 or side1 == side3:
-        print("The triangle is isosceles")
+        print("Isosceles")
     # scalene
     else:
-        print("The triangle is scalene")
-
+        print("Scalene")
 
 main()
