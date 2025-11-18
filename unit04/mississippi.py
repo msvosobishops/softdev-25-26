adc01e8c9326f62b4711078bee6da889d14762c4
msvosobishops <dominique.voso@bishops.com>
2025-11-18 09:27:58 -0800

    mississippi.py

--- unit04/mississippi.py
+++ unit04/mississippi.py
@@ -1,24 +1,33 @@
+'''
+Program that uses parallel lists to count how many times each character appears in a string
+'''
+
+
 def main():
     string = input("Enter a string: ")
-    unique_letters = []
-    letter_counts = []
+    letters = []
+    counts = []
 
     for char in string.lower():
         # add a unique letter to the list 
-        if char not in unique_letters:
-            unique_letters.append(char)
-            letter_counts.append(1)
+        if char not in letters:
+            letters.append(char)
+            counts.append(1)
         # add to the letter counts if the letter is not unique 
         else:
-            # get the position the char appears in the list 
-            i = unique_letters.index(char)
-            # add to corresponding position in letter counts 
-            letter_counts[i] += 1
+            # find the position where the char occurs 
+            position = letters.index(char)
+            # modify the count for that letter 
+            counts[position] += 1
+
+    # print everything nicely 
+    # headers 
+    print(f"{'Letters':<10} Counts")
+    # letters and counts
+    for i in range(len(letters)):
+        print(f"{letters[i]:<10} {counts[i]}")
+            
     
-    # display nicely
-    print(f"{'Letter':<20} Count")
-    for i in range(len(unique_letters)):
-        print(f"{unique_letters[i]:<20} {letter_counts[i]}")
         
 
 
---
+++ unit04/movie_analysis.py
@@ -0,0 +1,37 @@
+'''
+IMPORTANT: Make sure this file and the movies_data file are in the same folder 
+'''
+
+# imports the movies list and the all_ratings list 
+from movie_data import *
+
+def main():
+    pass    # delete this line once you begin programming
+
+    # 1. Create a new list called avg_ratings that has each movie's average rating 
+
+
+
+    # 2. Print out each movie and its average rating in nice aligned columns (use f-strings)
+    # Ex. Movie             Rating
+    #     Inception         9.10
+    #     Finding Nemo      8.90
+
+
+
+
+    # 3. Use python to determine: Which movie has the highest average rating? Print it out. 
+
+
+
+    # 4. Which movie is mid? Use python to find the movie whose average rating is approx the middle of the pack and print it out. 
+
+
+
+    # 5. Ask the user for a movie and then tell them the highest and lowest rating that critics gave that movie. 
+    # This shouldn't crash if the entered movie isn't in the list; instead it should ask the user again until they enter a movie that is in the list
+    
+
+
+
+main()
